840e07f21fc906165dfd05136896f738d5b03b77
Cihat Talat <cihat.talat513@gmail.com>
2022-01-29 12:56:46 +0300

    api_services

--- lib/services/news_services_api.dart
+++ lib/services/news_services_api.dart
@@ -5,7 +5,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:news_app/models/news_api_model.dart';
 
 class NewsApiServices{
-  static String _apiKey= "853ac7418d3a480e94a8115c18edf3c6";
+  static String _apiKey= "";
   static final String _url = "https://newsapi.org/v2/top-headlines?country=tr&apiKey=$_apiKey";
 
   static Future<List<Article>> fetchNewsArticle() async{
